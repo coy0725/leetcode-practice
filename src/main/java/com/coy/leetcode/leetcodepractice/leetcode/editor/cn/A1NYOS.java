f83b83043744d4533ef712dc3c9a31d0eadb8050
coy0725 <799057410@qq.com>
2022-09-27 22:35:43 +0800

    剑指 Offer II 011. 0 和 1 个数相同的子数组

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/A1NYOS.java
@@ -0,0 +1,72 @@
+/**
+ * 0 和 1 个数相同的子数组
+ */
+//给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: nums = [0,1]
+//输出: 2
+//说明: [0, 1] 是具有相同数量 0 和 1 的最长连续子数组。 
+//
+// 示例 2： 
+//
+// 
+//输入: nums = [0,1,0]
+//输出: 2
+//说明: [0, 1] (或 [1, 0]) 是具有相同数量 0 和 1 的最长连续子数组。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 105 
+// nums[i] 不是 0 就是 1 
+// 
+//
+// 
+//
+// 注意：本题与主站 525 题相同： https://leetcode-cn.com/problems/contiguous-array/ 
+// Related Topics 数组 哈希表 前缀和 
+// 👍 99 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.HashMap;
+
+/**
+ * 剑指 Offer II 011
+ */
+public class A1NYOS {
+    public static void main(String[] args) {
+        Solution solution = new A1NYOS().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findMaxLength(int[] nums) {
+            //key:数组下标的子数组之和，value:数组下标
+            HashMap<Integer, Integer> sumToIndex = new HashMap<>();
+            int maxLength = 0;
+            int sum = 0;
+
+            sumToIndex.put(0, -1);
+            for (int i = 0; i < nums.length; i++) {
+                int value = nums[i]==0?-1:1;
+                sum =sum+value;//当前子数组的和
+                if (sumToIndex.containsKey(sum)){//之前子数组的和与当前子数组的和相等
+                    maxLength = Math.max(maxLength,i - sumToIndex.get(sum));
+                }else {
+                    sumToIndex.put(sum,i);
+                }
+            }
+            return maxLength;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MoveZeroes.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MoveZeroes.java
@@ -1,7 +1,6 @@
 /**
-* 移动零
-* 
-*/
+ * 移动零
+ */
 //给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
 //
 // 示例: 
@@ -17,28 +16,30 @@
 // 
 // Related Topics 数组 双指针 
 // 👍 1236 👎 0
-	
+
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
 /**
-* 283
-*/
+ * 283
+ */
 public class MoveZeroes {
     public static void main(String[] args) {
         Solution solution = new MoveZeroes().new Solution();
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void moveZeroes(int[] nums) {
-        //i与i+1比较，如果i+1为0，i不为0,则两个位置的元素进行交换
-        for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i]!=0&&nums[i+1]==0){
-                int temp = nums[i];
-                nums[i] = nums[i+1];
-                nums[i+1] = temp;
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            //i与i+1比较，如果i+1为0，i不为0,则两个位置的元素进行交换
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] != 0 && nums[i + 1] == 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                }
             }
         }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
