68ebe8a96fc2d9901db1d510a4a0c91b2aaf2cb7
coy0725 <799057410@qq.com>
2022-10-04 19:31:00 +0800

    53. 最大子数组和 动态规划

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MaximumSubarray.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MaximumSubarray.java
@@ -1,7 +1,6 @@
 /**
-* 最大子数组和
-* 
-*/
+ * 最大子数组和
+ */
 //给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。 
 //
 // 子数组 是数组中的一个连续部分。 
@@ -44,39 +43,51 @@
 // 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。 
 // Related Topics 数组 分治 动态规划 
 // 👍 5350 👎 0
-	
+
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
 /**
-* 53
-*/
+ * 53
+ */
 public class MaximumSubarray {
     public static void main(String[] args) {
         Solution solution = new MaximumSubarray().new Solution();
-        System.out.println(solution.maxSubArray(new int[] {-2,-1}));
+        System.out.println(solution.maxSubArray(new int[] {-2, -1}));
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxSubArray(int[] nums) {
-        int[] sums = new int[nums.length];
-        sums[0]=nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sums[i]=sums[i-1]+nums[i];
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int[] sums = new int[nums.length];
+            sums[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                sums[i] = sums[i - 1] + nums[i];
+            }
+
+            int maxSum = sums[0];
+            int minValue = sums[0];
+            for (int i = 0; i < sums.length; i++) {
+                if (sums[i] < minValue) {
+                    minValue = sums[i];
+                } else if (sums[i] - minValue > maxSum) {
+                    maxSum = sums[i] - minValue;
+                }
+            }
+            return maxSum;
+
         }
 
-        int maxSum=sums[0];
-        int minValue=sums[0];
-        for (int i = 0; i < sums.length; i++) {
-            if (sums[i]<minValue){
-                minValue=sums[i];
+        public int maxSubArray2(int[] nums) {
+            int pre = 0;
+            int res = nums[0];
+            for (int num : nums) {
+                pre = Math.max(pre + num, num);
+                res = Math.max(res, pre);
             }
-             else if (sums[i]-minValue>maxSum){
-                maxSum=sums[i]-minValue;
-            }
+            return res;
         }
-        return maxSum;
 
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
