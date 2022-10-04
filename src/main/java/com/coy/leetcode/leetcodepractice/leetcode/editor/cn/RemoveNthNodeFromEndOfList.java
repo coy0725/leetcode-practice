c3251ace3a455269f3705dbc9f6b37368b9d888b
coy0725 <799057410@qq.com>
2022-10-04 17:31:03 +0800

    面试题21：删除倒数第k个节点

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,102 @@
+/**
+ * 删除链表的倒数第 N 个结点
+ */
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针 
+// 👍 2246 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+/**
+ * 19
+ */
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode();
+            dummy.next=head;
+
+            ListNode firstNode = dummy;
+            ListNode secondNode = dummy;
+            while (firstNode.next!=null){
+                n--;
+                if (n<0){
+                    secondNode = secondNode.next;
+                }
+                firstNode=firstNode.next;
+            }
+            secondNode.next=secondNode.next.next;
+            return dummy.next;
+
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) { this.val = val; }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
