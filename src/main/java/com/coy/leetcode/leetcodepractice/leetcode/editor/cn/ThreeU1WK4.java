da2a70c6eb3dc368aa387f8be5c70e1e7c694636
coy0725 <799057410@qq.com>
2022-10-06 12:51:16 +0800

    160. 相交链表 不通过，修改了数组

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ThreeU1WK4.java
@@ -0,0 +1,160 @@
+/**
+ * 两个链表的第一个重合节点
+ */
+//给定两个单链表的头节点 headA 和 headB ，请找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。 
+//
+// 图示两个链表在节点 c1 开始相交： 
+//
+// 
+//
+// 题目数据 保证 整个链式结构中不存在环。 
+//
+// 注意，函数返回结果后，链表必须 保持其原始结构 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, sk
+//ipB = 3
+//输出：Intersected at '8'
+//解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+//从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。
+//在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 
+//1
+//输出：Intersected at '2'
+//解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+//从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。
+//在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+//输出：null
+//解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+//由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+//这两个链表不相交，因此返回 null 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// listA 中节点数目为 m 
+// listB 中节点数目为 n 
+// 0 <= m, n <= 3 * 104 
+// 1 <= Node.val <= 105 
+// 0 <= skipA <= m 
+// 0 <= skipB <= n 
+// 如果 listA 和 listB 没有交点，intersectVal 为 0 
+// 如果 listA 和 listB 有交点，intersectVal == listA[skipA + 1] == listB[skipB + 1] 
+// 
+//
+// 
+//
+// 进阶：能否设计一个时间复杂度 O(n) 、仅用 O(1) 内存的解决方案？ 
+//
+// 
+//
+// 注意：本题与主站 160 题相同：https://leetcode-cn.com/problems/intersection-of-two-linked-
+//lists/ 
+// Related Topics 哈希表 链表 双指针 
+// 👍 51 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * 剑指 Offer II 023
+ */
+public class ThreeU1WK4 {
+    public static void main(String[] args) {
+        Solution solution = new ThreeU1WK4().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode nodeA = reverseList(headA);
+            ListNode nodeB = reverseList(headB);
+            ListNode tmpHeadA = nodeA;
+            ListNode tmpHeadB = nodeB;
+
+            if (nodeA != nodeB) {
+                reverseList(tmpHeadA);
+                reverseList(tmpHeadB);
+                return null;
+            }
+            ListNode intersectionNode = nodeA;
+
+            while (nodeA != null && nodeB != null && nodeA.val == nodeB.val) {
+                intersectionNode = nodeA;
+                nodeA = nodeA.next;
+                nodeB = nodeB.next;
+            }
+            reverseList(tmpHeadA);
+            reverseList(tmpHeadB);
+            return intersectionNode;
+        }
+
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode cur = head;
+            ListNode pre = null;
+
+            while (cur != null) {
+                ListNode next = cur.next;//保存当前节点的下一个节点
+                cur.next = pre;//反转当前节点的指向
+                pre = cur;//记录下一个节点的前一个节点
+                cur = next;//更新下一个节点的当前节点
+            }
+            return pre;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) { this.val = val; }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
