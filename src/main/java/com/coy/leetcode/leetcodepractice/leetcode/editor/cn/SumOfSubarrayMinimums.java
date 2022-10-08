99fbbacc48744bbd2fa8f52b89b69d597dba8dea
coy0725 <799057410@qq.com>
2022-10-08 21:55:31 +0800

    907. 子数组的最小值之和 正确解答

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/SumOfSubarrayMinimums.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/SumOfSubarrayMinimums.java
@@ -39,7 +39,11 @@
 	
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
 
 /**
 * 907
@@ -51,6 +55,7 @@ public class SumOfSubarrayMinimums {
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+        private static final int MOD = 1000000007;
     public int sumSubarrayMins(int[] arr) {
 
         //step1 获取所有的子数组
@@ -61,12 +66,12 @@ class Solution {
 
         for (int i = 0; i < arr.length; i++) {
             int minValue=arr[i];
-            sum = sum+minValue;
+            sum = (sum+minValue)% MOD;
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[j]<minValue){
                     minValue=arr[j];
                 }
-                sum=sum+minValue;
+                sum=(sum+minValue)% MOD;
                 System.out.println(sum);
             }
         }
@@ -74,6 +79,44 @@ class Solution {
 
     }
 }
+
+    class Solution2 {
+        private static final int MOD = 1000000007;
+
+        // 重写根据下标取值方法，-1和n返回MIN_VALUE
+        private int getElement(int[] arr, int n, int i) {
+            if (i == -1 || i == n) {
+                return Integer.MIN_VALUE;
+            }
+            return arr[i];
+        }
+
+        public int sumSubarrayMins(int[] arr) {
+            // 处理边界情况
+            if (arr == null || arr.length == 0) {
+                return 0;
+            }
+            int n = arr.length;
+            long ans = 0;
+            Deque<Integer> stack = new LinkedList<>();
+            // 将下标-1和n作为两个哨兵元素，它们对应的元素为MIN_VALUE
+            // -1作为最左边界，n作为最右边界
+            for (int i = -1; i <= n; i++) {
+                // 向左寻找第一个小于等于A[i]的元素
+                while (!stack.isEmpty() && getElement(arr, n, stack.peek()) > getElement(arr, n, i)) {
+                    // A[cur]就是之前思路中的A[i]，注意区分和上面代码的区别
+                    // 对于每个出栈元素来说，i就是它们的右边界，而栈顶元素就是左边界
+                    int cur = stack.pop();
+                    // 计算贡献值
+                    ans = (ans + (long)(cur - stack.peek()) * (i - cur) * arr[cur]) % MOD;
+                }
+                stack.push(i);
+            }
+
+            return (int)ans;
+        }
+    }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
