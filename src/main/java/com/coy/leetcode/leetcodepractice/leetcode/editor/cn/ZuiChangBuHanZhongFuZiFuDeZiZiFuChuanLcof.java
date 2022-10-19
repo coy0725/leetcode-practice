e56908b3ca8a6da7cb152fe61c91c7444c8f56d0
coy0725 <799057410@qq.com>
2022-10-19 22:51:59 +0800

    剑指 Offer 48. 最长不含重复字符的子字符串 错误

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ZuiChangBuHanZhongFuZiFuDeZiZiFuChuanLcof.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ZuiChangBuHanZhongFuZiFuDeZiZiFuChuanLcof.java
@@ -1,7 +1,6 @@
 /**
-* 最长不含重复字符的子字符串
-* 
-*/
+ * 最长不含重复字符的子字符串
+ */
 //请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。 
 //
 // 
@@ -40,71 +39,109 @@
 //eating-characters/ 
 // Related Topics 哈希表 字符串 滑动窗口 
 // 👍 502 👎 0
-	
+
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 
 import java.util.HashMap;
 
 /**
-* 剑指 Offer 48
-*/
+ * 剑指 Offer 48
+ */
 public class ZuiChangBuHanZhongFuZiFuDeZiZiFuChuanLcof {
     public static void main(String[] args) {
         Solution solution = new ZuiChangBuHanZhongFuZiFuDeZiZiFuChuanLcof().new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println(solution.lengthOfLongestSubstring3("abcabcabc"));
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length()<=1){
-            return s.length();
-        }
-        int maxlength = 0;
-        int left = 0;
-        HashMap<Character,Integer> keyToCount = new HashMap<>();
-        for (int right = 0; right < s.length(); right++) {
-
-            keyToCount.put(s.charAt(right),keyToCount.getOrDefault(s.charAt(right),0)+1);
-            while (left<=right&&!areAllLessOne(keyToCount)){
-
-                keyToCount.put(s.charAt(left),keyToCount.getOrDefault(s.charAt(left),0)-1);
-                left++;
-            }
-            maxlength = Math.max(maxlength,right-left+1);
-        }
-        return maxlength;
-    }
-        public int lengthOfLongestSubstring2(String s) {
-            if (s.length()<=1){
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s.length() <= 1) {
                 return s.length();
             }
             int maxlength = 0;
             int left = 0;
-            int dupCount=0;//重复字符数
-            HashMap<Character,Integer> keyToCount = new HashMap<>();
+            HashMap<Character, Integer> keyToCount = new HashMap<>();
             for (int right = 0; right < s.length(); right++) {
-                Integer charCount = keyToCount.getOrDefault(s.charAt(right), 0);
-                //字符出现次数1-》2,重复字符数++
-                if (charCount==1){
-                    dupCount++;
-                }
-                keyToCount.put(s.charAt(right),charCount+1);
-                while (left<=right&&dupCount>0){
-                    Integer charCount2 = keyToCount.getOrDefault(s.charAt(left), 0);
-                    //字符出现次数2-》1,重复字符数--
-                    if (charCount2==2){
-                        dupCount--;
-                    }
-                    keyToCount.put(s.charAt(left), charCount2 -1);
+
+                keyToCount.put(s.charAt(right), keyToCount.getOrDefault(s.charAt(right), 0) + 1);
+                while (left <= right && !areAllLessOne(keyToCount)) {
+
+                    keyToCount.put(s.charAt(left), keyToCount.getOrDefault(s.charAt(left), 0) - 1);
                     left++;
                 }
-                maxlength = Math.max(maxlength,right-left+1);
+                maxlength = Math.max(maxlength, right - left + 1);
             }
             return maxlength;
         }
+
+        //字节面试时做错的样子
+        public int lengthOfLongestSubstring3(String s) {
+            int left =0;
+            int right =0;
+            int maxLength=0;
+            int duplicateCharCount=0;
+
+            HashMap<Character,Integer> charToCount = new HashMap<>();
+            for (right = 0; right <s.length() ; right++) {
+                Integer count = charToCount.getOrDefault(s.charAt(right), 0);
+                if (count==1){
+                    duplicateCharCount++;
+                }
+                charToCount.put(s.charAt(right),count+1);
+                if (duplicateCharCount<1){
+                    maxLength = Math.max(maxLength,right-left+1);
+                }else {
+                    for (int i = left; i < right; i++) {
+                        Integer n = charToCount.get(s.charAt(left));
+                        if (n==2){
+                            duplicateCharCount--;
+                        }
+                        charToCount.put(s.charAt(left),n-1);
+                        if (duplicateCharCount<1){
+                            maxLength=Math.max(maxLength,right-left+1);
+                            break;
+                        }
+                        left++;
+                    }
+                }
+
+            }
+            return maxLength;
+        }
+
+        public int lengthOfLongestSubstring2(String s) {
+            if (s.length() <= 1) {
+                return s.length();
+            }
+            int maxlength = 0;
+            int left = 0;
+            int dupCount = 0;//重复字符数
+            HashMap<Character, Integer> keyToCount = new HashMap<>();
+            for (int right = 0; right < s.length(); right++) {
+                Integer charCount = keyToCount.getOrDefault(s.charAt(right), 0);
+                //字符出现次数1-》2,重复字符数++
+                if (charCount == 1) {
+                    dupCount++;
+                }
+                keyToCount.put(s.charAt(right), charCount + 1);
+                while (left <= right && dupCount > 0) {
+                    Integer charCount2 = keyToCount.getOrDefault(s.charAt(left), 0);
+                    //字符出现次数2-》1,重复字符数--
+                    if (charCount2 == 2) {
+                        dupCount--;
+                    }
+                    keyToCount.put(s.charAt(left), charCount2 - 1);
+                    left++;
+                }
+                maxlength = Math.max(maxlength, right - left + 1);
+            }
+            return maxlength;
+        }
+
         private boolean areAllLessOne(HashMap<Character, Integer> keyToCount) {
             for (final Integer count : keyToCount.values()) {
-                if (count>1){
+                if (count > 1) {
                     return false;
                 }
             }
@@ -112,6 +149,6 @@ class Solution {
         }
 
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
