12106ab9cfcb3e69f75f886cc94da5752533aaf5
coy0725 <799057410@qq.com>
2022-10-27 23:34:34 +0800

    剑指 Offer II 046. 二叉树的右侧视图
    空指针异常

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/WNC0Lk.java
@@ -0,0 +1,123 @@
+/**
+* 二叉树的右侧视图
+* 
+*/
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 注意：本题与主站 199 题相同：https://leetcode-cn.com/problems/binary-tree-right-side-view
+/// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 37 👎 0
+	
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+* 剑指 Offer II 046
+*/
+public class WNC0Lk {
+    public static void main(String[] args) {
+        Solution solution = new WNC0Lk().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) { this.val = val; }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> rlt = new ArrayList<>();
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        Queue<TreeNode> temp;
+        queue1.offer(root);
+        while (!queue1.isEmpty()){
+            TreeNode node = queue1.poll();
+            if (node.left!=null){
+                queue2.offer(node.left);
+            }
+            if (node.right!=null){
+                queue2.offer(node.right);
+            }
+            if (queue1.isEmpty()){
+                rlt.add(node.val);
+                temp =queue1;
+                queue1 =queue2;
+                queue2 = temp;
+            }
+
+
+        }
+        return rlt;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
