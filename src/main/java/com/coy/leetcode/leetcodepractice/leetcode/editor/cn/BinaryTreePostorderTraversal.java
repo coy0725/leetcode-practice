9bd6d44abbd10e7e850ae1aa21f54dc24ef213e5
coy0725 <799057410@qq.com>
2022-10-31 22:53:28 +0800

    145. 二叉树的后序遍历
    第四遍 错误

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinaryTreePostorderTraversal.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -152,6 +152,35 @@ public class BinaryTreePostorderTraversal {
             return nodes;
         }
 
+        public List<Integer> postorderTraversal4(TreeNode root){
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            TreeNode prev = null;
+
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+
+                }
+
+                cur = stack.peek();
+
+                if (cur.right!=null && cur.right!=prev){
+                    stack.push(cur.right);
+                    cur = cur.right;
+                }else {
+                    cur = stack.pop();
+                    nodes.add(cur.val);
+                    prev = cur;
+                    cur =null;
+                }
+
+            }
+            return nodes;
+        }
+
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
