662b611aa9ebff34daf5656e2b318d6cb089dba4
coy0725 <799057410@qq.com>
2022-11-03 22:51:09 +0800

    剑指 Offer II 052. 展平二叉搜索树 第一次解答错误 输入 [5,3,6,2,4,null,8,1,null,null,null,7,9] 输出 [5,null,6,null,7,null,8,null,9] 预期结果 [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/NYBBNL.java
@@ -0,0 +1,110 @@
+/**
+* 展平二叉搜索树
+* 
+*/
+//给你一棵二叉搜索树，请 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的取值范围是 [1, 100] 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+//
+// 注意：本题与主站 897 题相同： https://leetcode-cn.com/problems/increasing-order-search-tr
+//ee/ 
+// Related Topics 栈 树 深度优先搜索 二叉搜索树 二叉树 
+// 👍 45 👎 0
+	
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+* 剑指 Offer II 052
+*/
+public class NYBBNL {
+    public static void main(String[] args) {
+        Solution solution = new NYBBNL().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        //step1:通过中序遍历，将节点从小到大存储到链表里
+        List<TreeNode> nodes = inOrderDfs(root);
+        if (nodes.size()==0||nodes.size()==1){
+            return root;
+        }
+        //step2:遍历链表里的节点，将节点左指针置空，右指针指向链表中的下一个节点
+        for (int i = 0; i < nodes.size()-1; i++) {
+            TreeNode cur = nodes.get(i);
+            TreeNode next = nodes.get(i + 1);
+            cur.left=null;
+            cur.right =next;
+        }
+        return root;
+    }
+
+    private List<TreeNode> inOrderDfs(TreeNode root) {
+        List<TreeNode> nodes = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            nodes.add(cur);
+            cur = cur.right;
+        }
+
+        return nodes;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
