082e606e72cf389627f847f17dee50dcd7c3c288
coy0725 <799057410@qq.com>
2022-11-04 23:24:40 +0800

    Revert "Revert "98. 验证二叉搜索树 第一次解答错误 输入： [2,2,2] 输出： true 预期结果： false""
    This reverts commit f126f599

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,102 @@
+/**
+* 验证二叉搜索树
+* 
+*/
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 104] 内 
+// -231 <= Node.val <= 231 - 1 
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 
+// 👍 1801 👎 0
+	
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.Stack;
+
+/**
+* 98
+*/
+public class ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new ValidateBinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+
+        //使用中序遍历，使用一个变量保存之前访问的节点
+        boolean isValidBST = true;
+
+        TreeNode cur = root;
+        TreeNode prev = null;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (cur != null || !stack.isEmpty()) {
+            while (cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            //如果前一个节点访问的值大于当前访问节点的值，说明不是搜索树
+            if (prev!=null&&prev.val>cur.val){
+                isValidBST = false;
+                break;
+            }
+            prev = cur;
+            cur = cur.right;
+
+        }
+
+        return isValidBST;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
