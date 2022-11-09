1ef5996271c0ba8bfd7273ccdd246fa2adc78d9f
coy0725 <799057410@qq.com>
2022-11-10 00:10:01 +0800

    剑指 Offer II 068. 查找插入位置 第一次解答 修正，需要判断当前mid位置是不是大于目标值，不大于的话返回mid+1

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/N6YdxV.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/N6YdxV.java
@@ -68,6 +68,7 @@ package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 public class N6YdxV {
     public static void main(String[] args) {
         Solution solution = new N6YdxV().new Solution();
+        System.out.println(solution.searchInsert(new int[] {1, 3, 5, 6}, 2));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
@@ -87,7 +88,10 @@ class Solution {
                 left = mid+1;
             }
         }
-        return mid;
+
+        if (nums[mid] > target) {
+            return mid;
+        }else return mid+1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
