553254cb25bd2631a444b6b3d8b7484d0aa1015c
coy0725 <799057410@qq.com>
2022-11-10 23:58:29 +0800

    剑指 Offer II 039. 直方图最大矩形面积 第一次解答，看了题解

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ZeroYnMMM.java
@@ -0,0 +1,88 @@
+/**
+ * 直方图最大矩形面积
+ */
+//给定非负整数数组 heights ，数组中的数字用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。 
+//
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：heights = [2,1,5,6,2,3]
+//输出：10
+//解释：最大的矩形为图中红色区域，面积为 10
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入： heights = [2,4]
+//输出： 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= heights.length <=105 
+// 0 <= heights[i] <= 104 
+// 
+//
+// 
+//
+// 注意：本题与主站 84 题相同： https://leetcode-cn.com/problems/largest-rectangle-in-histog
+//ram/ 
+// Related Topics 栈 数组 单调栈 
+// 👍 63 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.Stack;
+
+/**
+ * 剑指 Offer II 039
+ */
+public class ZeroYnMMM {
+    public static void main(String[] args) {
+        Solution solution = new ZeroYnMMM().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int largestRectangleArea(int[] heights) {
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+            int maxArea = 0;
+            for (int i = 0; i < heights.length; i++) {
+                //当前柱子高度小于栈顶柱子高度
+                while (stack.peek() != -1 && heights[i] < heights[stack.peek()]) {
+                    //计算以栈顶柱子高度为高的最大面积
+                    int height = heights[stack.pop()];
+                    //i是当前柱子右边，stack.peek是当前柱子左边
+                    int width = i - stack.peek() - 1;
+                    int area = height * width;
+
+                    maxArea = Math.max(maxArea, area);
+                }
+                stack.push(i);
+            }
+
+            while (stack.peek() != -1) {
+                int height = heights[stack.pop()];
+                //i小于length，为什么这里的宽用的右边使用height.length?
+                int width = heights.length - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            return maxArea;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
