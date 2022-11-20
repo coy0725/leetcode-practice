faab1771558818008af6b43140c1ab870e401f9c
coy0725 <799057410@qq.com>
2022-11-20 23:28:32 +0800

    39. 组合总和 第一遍 看了题解

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/CombinationSum.java
@@ -0,0 +1,99 @@
+/**
+ * 组合总和
+ */
+//给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的
+// 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。 
+//
+// candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+//
+// 对于给定的输入，保证和为 target 的不同组合数少于 150 个。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：candidates = [2,3,6,7], target = 7
+//输出：[[2,2,3],[7]]
+//解释：
+//2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+//7 也是一个候选， 7 = 7 。
+//仅有这两种组合。 
+//
+// 示例 2： 
+//
+// 
+//输入: candidates = [2,3,5], target = 8
+//输出: [[2,2,2,2],[2,3,3],[3,5]] 
+//
+// 示例 3： 
+//
+// 
+//输入: candidates = [2], target = 1
+//输出: []
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= candidates.length <= 30 
+// 2 <= candidates[i] <= 40 
+// candidates 的所有元素 互不相同 
+// 1 <= target <= 40 
+// 
+// Related Topics 数组 回溯 
+// 👍 2255 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 39
+ */
+public class CombinationSum {
+    public static void main(String[] args) {
+        Solution solution = new CombinationSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            int len = candidates.length;
+            List<List<Integer>> res = new ArrayList<>();
+            if (len == 0) {
+                return res;
+            }
+
+            Deque<Integer> path = new ArrayDeque<>();
+            dfs(candidates, 0, len, target, path, res);
+            return res;
+        }
+
+        private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path,
+            List<List<Integer>> res) {
+            if (target < 0) {
+                return;
+            }
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+            }
+
+            for (int i = begin; i < len; i++) {
+                path.addLast(candidates[i]);
+                // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
+                dfs(candidates, i, len, target - candidates[i], path, res);
+
+                path.removeLast();
+            }
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
