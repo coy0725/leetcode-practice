67917ffc01d0fb3bbdd0c955cb690651f56f66c1
coy0725 <799057410@qq.com>
2022-11-28 23:11:51 +0800

    剑指 Offer II 088. 爬楼梯的最少成本  空间复杂度为O（n）的迭代代码

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/GzCJIP.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/GzCJIP.java
@@ -90,4 +90,21 @@ public class GzCJIP {
 
         }
     }
+
+    class Solution3 {
+        public int minCostClimbingStairs(int[] cost) {
+
+            int len = cost.length;
+            int[] dp = new int[len];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for (int i = 2; i < len; i++) {
+                dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
+            }
+            return Math.min(dp[len-2], dp[len-1]);
+
+        }
+
+
+    }
 }
