b1dc0309887b94b436c5650cf4c5b858b2b60934
coy0725 <799057410@qq.com>
2022-11-29 08:35:41 +0800

    剑指 Offer II 089. 房屋偷盗 //空间复杂度为O（1）的迭代代码 哦 自己写出来了

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/Gu0c2T.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/Gu0c2T.java
@@ -49,7 +49,7 @@ import java.util.Arrays;
  */
 public class Gu0c2T {
     public static void main(String[] args) {
-        Solution2 solution = new Gu0c2T().new Solution2();
+        Solution3 solution = new Gu0c2T().new Solution3();
         System.out.println(solution.rob(new int[] {2,7,9,3,1}));
     }
 
@@ -114,6 +114,32 @@ public class Gu0c2T {
                 dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
             }
             return dp[nums.length - 1];
+        }
+
+
+
+    }
+
+    //空间复杂度为O（1）的迭代代码
+    class Solution3 {
+        public int rob(int[] nums) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[2];
+            dp[0] = nums[0];
+            if (nums.length > 1) {
+                dp[1] = Math.max(nums[0], nums[1]);
+            }
+            for (int i = 2; i < nums.length; i++) {
+                if (i % 2 == 0) {
+                    dp[i%2] = Math.max(dp[1], dp[0] + nums[i]);
+                } else {
+                    dp[i%2] = Math.max(dp[0], dp[1] + nums[i]);
+                }
+
+            }
+            return Math.max(dp[0], dp[1]);
         }
 
 
