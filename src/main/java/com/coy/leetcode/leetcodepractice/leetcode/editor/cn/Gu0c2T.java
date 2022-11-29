8baac1a9bbbe8d8f42ef8b8fcb16063d3a5e846e
coy0725 <799057410@qq.com>
2022-11-29 08:37:33 +0800

    剑指 Offer II 089. 房屋偷盗 //空间复杂度为O（1）的迭代代码 优化，我想的是用+法，但是会越界，怎么没想着用减法呢

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/Gu0c2T.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/Gu0c2T.java
@@ -132,11 +132,7 @@ public class Gu0c2T {
                 dp[1] = Math.max(nums[0], nums[1]);
             }
             for (int i = 2; i < nums.length; i++) {
-                if (i % 2 == 0) {
-                    dp[i%2] = Math.max(dp[1], dp[0] + nums[i]);
-                } else {
-                    dp[i%2] = Math.max(dp[0], dp[1] + nums[i]);
-                }
+                dp[i%2] = Math.max(dp[(i-1)%2], dp[(i-2)%2] + nums[i]);
 
             }
             return Math.max(dp[0], dp[1]);
