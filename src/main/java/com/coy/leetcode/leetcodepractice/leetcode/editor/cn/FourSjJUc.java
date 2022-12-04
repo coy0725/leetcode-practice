671480548b2a7f83cb68cf84a37a6736286d90d1
coy0725 <799057410@qq.com>
2022-12-04 22:39:42 +0800

    剑指 Offer II 082. 含有重复元素集合的组 第三遍 错误 [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]] []

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/FourSjJUc.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/FourSjJUc.java
@@ -60,6 +60,9 @@ public class FourSjJUc {
     public static void main(String[] args) {
         Solution solution = new FourSjJUc().new Solution();
         System.out.println(solution.combinationSum2(new int[] {10, 1, 2, 7, 6, 1, 5}, 8));
+
+        Solution3 solution3 = new FourSjJUc().new Solution3();
+        System.out.println(solution3.combinationSum(new int[] {10, 1, 2, 7, 6, 1, 5}, 8));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -86,7 +89,7 @@ public class FourSjJUc {
                 dfs(res, candidates, combination, target, sum + candidates[i], i + 1);
                 combination.remove(combination.size() - 1);
 
-                dfs(res, candidates, combination, target, sum , getNext(candidates,i));
+                dfs(res, candidates, combination, target, sum, getNext(candidates, i));
             }
         }
 
@@ -106,35 +109,70 @@ public class FourSjJUc {
         LinkedList<Integer> path = new LinkedList<>();
         int sum = 0;
 
-        public List<List<Integer>> combinationSum2( int[] candidates, int target ) {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
             //为了将重复的数字都放到一起，所以先进行排序
-            Arrays.sort( candidates );
-            backTracking( candidates, target, 0 );
+            Arrays.sort(candidates);
+            backTracking(candidates, target, 0);
             return res;
         }
 
-        private void backTracking( int[] candidates, int target, int start ) {
-            if ( sum == target ) {
-                res.add( new ArrayList<>( path ) );
+        private void backTracking(int[] candidates, int target, int start) {
+            if (sum == target) {
+                res.add(new ArrayList<>(path));
                 return;
             }
-            for ( int i = start; i < candidates.length && sum + candidates[i] <= target; i++ ) {
+            for (int i = start; i < candidates.length && sum + candidates[i] <= target; i++) {
                 //正确剔除重复解的办法
                 //跳过同一树层使用过的元素
-                if ( i > start && candidates[i] == candidates[i - 1] ) {
+                if (i > start && candidates[i] == candidates[i - 1]) {
                     continue;
                 }
 
                 sum += candidates[i];
-                path.add( candidates[i] );
+                path.add(candidates[i]);
                 // i+1 代表当前组内元素只选取一次
-                backTracking( candidates, target, i + 1 );
+                backTracking(candidates, target, i + 1);
 
                 int temp = path.getLast();
                 sum -= temp;
                 path.removeLast();
             }
         }
+    }
+
+    class Solution3 {
+
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        int sum = 0;
+
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            //为了将重复的数字都放到一起，所以先进行排序
+            Arrays.sort(candidates);
+            backTracking(candidates, target, 0);
+            return res;
+        }
+
+        //candidates 中的每个数字在每个组合中只能使用一次，解集不能包含重复的组合。
+        private void backTracking(int[] candidates, int target, int i) {
+            if (sum == target) {
+                res.add(new LinkedList<>(path));
+            } else if (sum < target && i < candidates.length){
+
+                //避免选取了重复元素
+                if (i > 0 && candidates[i] == candidates[i - 1]) {
+                    return;
+                }
+                //选取当前元素
+                path.addLast(candidates[i]);
+                backTracking(candidates, target, i + 1);
+                path.removeLast();
+                //不选取当前元素
+                backTracking(candidates, target, i + 1);
+            }
+
+        }
+
     }
 
 }
