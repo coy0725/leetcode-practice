70900dc1d6ce7cb49add972af50ccae7d409efaa
coy0725 <799057410@qq.com>
2022-12-12 14:40:26 +0800

    剑指 Offer 27. 二叉树的镜像 第二遍

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ErChaShuDeJingXiangLcof.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ErChaShuDeJingXiangLcof.java
@@ -1,7 +1,6 @@
 /**
-* 二叉树的镜像
-* 
-*/
+ * 二叉树的镜像
+ */
 //请完成一个函数，输入一个二叉树，该函数输出它的镜像。 
 //
 // 例如输入： 
@@ -36,45 +35,58 @@
 // 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
 // Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
 // 👍 316 👎 0
-	
+
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-
 /**
-* 剑指 Offer 27
-*/
+ * 剑指 Offer 27
+ */
 public class ErChaShuDeJingXiangLcof {
     public static void main(String[] args) {
         Solution solution = new ErChaShuDeJingXiangLcof().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public TreeNode mirrorTree(TreeNode root) {
-        if (root==null){
-            return null;
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public TreeNode mirrorTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
+            TreeNode left = mirrorTree(root.left);
+            TreeNode right = mirrorTree(root.right);
+
+            root.right = left;
+            root.left = right;
+            return root;
         }
-
-        TreeNode left = mirrorTree(root.left);
-        TreeNode right = mirrorTree(root.right);
-
-        root.right = left;
-        root.left = right;
-        return root;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    class Solution2 {
+        public TreeNode mirrorTree(TreeNode root) {
+            if (root == null) {
+                return root;
+            }
+            //将左子树镜像
+            TreeNode left = mirrorTree(root.left);
+            //将右子树镜像
+            TreeNode right = mirrorTree(root.right);
+
+            //左右子树互换
+            root.right = left;
+            root.left = right;
+            return root;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
