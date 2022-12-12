17250866382b5c1347c7982d50972e6b1292caa3
coy0725 <799057410@qq.com>
2022-12-12 15:50:17 +0800

    94. 二叉树的中序遍历 第5遍

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinaryTreeInorderTraversal.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -1,7 +1,6 @@
 /**
-* 二叉树的中序遍历
-* 
-*/
+ * 二叉树的中序遍历
+ */
 //给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。 
 //
 // 
@@ -41,7 +40,7 @@
 // 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
 // Related Topics 栈 树 深度优先搜索 二叉树 
 // 👍 1605 👎 0
-	
+
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 
 import java.util.ArrayList;
@@ -49,141 +48,161 @@ import java.util.List;
 import java.util.Stack;
 
 /**
-* 94
-*/
+ * 94
+ */
 public class BinaryTreeInorderTraversal {
     public static void main(String[] args) {
         Solution solution = new BinaryTreeInorderTraversal().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> rlt = new ArrayList<>();
-        dfs(root,rlt);
-        return rlt;
-    }
 
-    private void dfs(TreeNode root, List<Integer> rlt) {
-        if (root !=null){
-            dfs(root.left,rlt);
-            rlt.add(root.val);
-            dfs(root.right,rlt);
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> rlt = new ArrayList<>();
+            dfs(root, rlt);
+            return rlt;
         }
-    }
 
-    public List<Integer> inorderTraversal2(TreeNode root) {
-        List<Integer> nodes = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        //定义一个当前正在访问的节点
-        TreeNode cur =root;
-        while (cur!=null || !stack.isEmpty()){
-            while (cur!=null){
-                stack.push(cur);
-                cur =cur.left;
+        private void dfs(TreeNode root, List<Integer> rlt) {
+            if (root != null) {
+                dfs(root.left, rlt);
+                rlt.add(root.val);
+                dfs(root.right, rlt);
             }
-            //先访问到最下层的最左节点，
-            cur = stack.pop();
-            nodes.add(cur.val);//访问最左节点
-            cur = cur.right;//这一句是什么意思呢，去掉和cur=null都是错误的
         }
 
-        //先访问左子树，再访问根节点，再访问右子树
-
-        return nodes;
-    }
-
-    public List<Integer> inorderTraversal3(TreeNode root){
-        List<Integer> nodes = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur;
-
-        cur =root;
-
-        while (cur!=null || !stack.isEmpty()){
-            while (cur!=null){
-                stack.push(cur);
-                cur = cur.left;
+        public List<Integer> inorderTraversal2(TreeNode root) {
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            //定义一个当前正在访问的节点
+            TreeNode cur = root;
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                //先访问到最下层的最左节点，
+                cur = stack.pop();
+                nodes.add(cur.val);//访问最左节点
+                cur = cur.right;//这一句是什么意思呢，去掉和cur=null都是错误的
             }
-            cur  = stack.pop();//当前正在访问的节点
-            nodes.add(cur.val);//访问当前节点
-            cur = cur.right;//访问当前节点的右节点
 
+            //先访问左子树，再访问根节点，再访问右子树
+
+            return nodes;
         }
-        return nodes;
-    }
 
-    public List<Integer> inorderTraversal4(TreeNode root){
-        List<Integer> nodes = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
+        public List<Integer> inorderTraversal3(TreeNode root) {
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur;
 
-        while (cur != null || !stack.isEmpty()) {
-            while (cur!=null){
-                stack.push(cur);
-                cur = cur.left;
+            cur = root;
+
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();//当前正在访问的节点
+                nodes.add(cur.val);//访问当前节点
+                cur = cur.right;//访问当前节点的右节点
+
             }
-            cur = stack.pop();
-            nodes.add(cur.val);
-            cur = cur.right;
-
-
+            return nodes;
         }
 
-        return nodes;
-    }
+        public List<Integer> inorderTraversal4(TreeNode root) {
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
 
-    public List<Integer> inorderTraversal5(TreeNode root){
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();
+                nodes.add(cur.val);
+                cur = cur.right;
 
-        List<Integer> nodes = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur =root;
-
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
             }
-            cur = stack.pop();
-            nodes.add(cur.val);//访问当前节点
-            cur = cur.right;//访问当前节点右节点
+
+            return nodes;
         }
 
-        return nodes;
-    }
+        public List<Integer> inorderTraversal5(TreeNode root) {
 
-    public List<Integer> inorderTraversal6(TreeNode root){
-        List<Integer> nodes = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur =root;
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();
+                nodes.add(cur.val);//访问当前节点
+                cur = cur.right;//访问当前节点右节点
             }
-            cur = stack.pop();
-            nodes.add(cur.val);
-            cur = cur.right;
 
+            return nodes;
         }
 
-        return nodes;
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+        public List<Integer> inorderTraversal6(TreeNode root) {
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();
+                nodes.add(cur.val);
+                cur = cur.right;
 
+            }
+
+            return nodes;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class Solution5 {
+        //左子树-根节点-右子树
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> nodes = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode curVisit = root;
+            while (curVisit != null || !stack.isEmpty()) {
+                while (curVisit != null) {
+                    stack.push(curVisit);
+                    curVisit = curVisit.left;
+                }
+                TreeNode pop = stack.pop();
+                //访问最左节点
+                nodes.add(pop.val);
+                curVisit = pop.right;
+            }
+            return nodes;
+        }
+
+    }
 }
