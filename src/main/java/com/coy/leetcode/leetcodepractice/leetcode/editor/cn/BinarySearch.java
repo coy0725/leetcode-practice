6d7dd62f4e9c7c8050fb58ff274ca62c645ccab6
coy0725 <799057410@qq.com>
2022-12-13 22:48:18 +0800

    704. 二分查找 fail 修正 输入 [5] 5 预期 0 实际 -1

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinarySearch.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/BinarySearch.java
@@ -50,7 +50,7 @@ public class BinarySearch {
             int left = 0;
             int right = nums.length - 1;
 
-            while (left < right) {
+            while (left <= right) {
                 int mid = left + (right - left) / 2;
                 if (nums[mid] == target) {
                     return mid;
