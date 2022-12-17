c391f1e39e67871bc84b64cc4dbb391b8c61c5b9
coy0725 <799057410@qq.com>
2022-12-17 15:31:59 +0800

    剑指 Offer II 095. 最长公共子序列  优化空间复杂度

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/QJnOS7.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/QJnOS7.java
@@ -129,4 +129,31 @@ public class QJnOS7 {
 
     }
 
+    /**
+     * 优化空间使用
+     */
+    class Solution3 {
+
+        public int longestCommonSubsequence(String text1, String text2) {
+            int len1 = text1.length();
+            int len2 = text2.length();
+            if (len1 == 0 || len2 == 0) {
+                return 0;
+            }
+            int[][] dp = new int[2][len2+1];
+            for (int i = 0; i < len1; i++) {
+                for (int j = 0; j < len2; j++) {
+                    if (text1.charAt(i) == text2.charAt(j)) {
+                        dp[(i + 1)%2][j + 1] = dp[i%2][j] + 1;
+                    } else {
+                        dp[(i + 1)%2][j + 1] = Math.max(dp[i%2][j + 1], dp[(i + 1)%2][j]);
+                    }
+                }
+            }
+            return dp[len1%2][len2];
+
+        }
+
+    }
+
 }
