60ef2d1088ff6037d86f1d8f668f6bf87e62d610
coy0725 <799057410@qq.com>
2022-12-17 15:25:43 +0800

    剑指 Offer II 095. 最长公共子序列 迭代法

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/QJnOS7.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/QJnOS7.java
@@ -105,4 +105,28 @@ public class QJnOS7 {
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
+    class Solution2 {
+
+        public int longestCommonSubsequence(String text1, String text2) {
+            int len1 = text1.length();
+            int len2 = text2.length();
+            if (len1 == 0 || len2 == 0) {
+                return 0;
+            }
+            int[][] dp = new int[len1 +1][len2 +1];
+            for (int i = 0; i < len1; i++) {
+                for (int j = 0; j < len2; j++) {
+                    if (text1.charAt(i) == text2.charAt(j)) {
+                        dp[i + 1][j + 1] = dp[i][j] + 1;
+                    } else {
+                        dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                    }
+                }
+            }
+            return dp[len1][len2];
+
+        }
+
+    }
+
 }
