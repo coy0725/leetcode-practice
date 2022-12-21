1c42264e7fb2893439064e5bc4069a37001e5a01
coy0725 <799057410@qq.com>
2022-12-22 00:42:53 +0800

    2114. 句子中的最多单词数 第一遍

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MaximumNumberOfWordsFoundInSentences.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/MaximumNumberOfWordsFoundInSentences.java
@@ -58,7 +58,7 @@ public class MaximumNumberOfWordsFoundInSentences {
         public int mostWordsFound(String[] sentences) {
             int max = 0;
             for (int i = 0; i < sentences.length; i++) {
-                int count = sentences[i].split("").length;
+                int count = sentences[i].split(" ").length;
 
                 max = Math.max(max, count);
             }
