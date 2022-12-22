88f36fa2701003f8f3428af6ed5dbca78d5953e3
coy0725 <799057410@qq.com>
2022-12-22 15:26:04 +0800

    146. LRU 缓存 手写双向链表+hashMap

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/LruCache.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/LruCache.java
@@ -57,6 +57,7 @@
 
 package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -111,6 +112,8 @@ public class LruCache {
         }
     }
 
+
+
     /**
      * Your LRUCache object will be instantiated and called as such:
      * LRUCache obj = new LRUCache(capacity);
@@ -119,4 +122,89 @@ public class LruCache {
      */
     //leetcode submit region end(Prohibit modification and deletion)
 
+}
+
+class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+
+        public DLinkedNode() {
+
+        }
+
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        moveToHead(node);
+        return node.value;
+    }
+
+
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            //添加到哈希表
+            cache.put(key, newNode);
+            addToHead(newNode);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = removeTailNode();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void removeNode(DLinkedNode node) {
+
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    private DLinkedNode removeTailNode() {
+        DLinkedNode eldestNode = tail.prev;
+        removeNode(eldestNode);
+        return eldestNode;
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
 }
