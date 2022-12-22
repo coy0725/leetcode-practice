5f4a3b4e02bffbff6ef6542540492a988a195c57
coy0725 <799057410@qq.com>
2022-12-22 14:57:51 +0800

    146. LRU 缓存 linkedHashMap

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/LruCache.java
@@ -0,0 +1,122 @@
+/**
+ * LRU 缓存
+ */
+//请你设计并实现一个满足 LRU (最近最少使用) 缓存 约束的数据结构。 
+//
+// 实现 LRUCache 类： 
+//
+// 
+// 
+// 
+// LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存 
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
+// void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 ke
+//y-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。 
+// 
+//
+// 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。 
+// 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//输出
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//解释
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // 缓存是 {1=1}
+//lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+//lRUCache.get(1);    // 返回 1
+//lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+//lRUCache.get(2);    // 返回 -1 (未找到)
+//lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+//lRUCache.get(1);    // 返回 -1 (未找到)
+//lRUCache.get(3);    // 返回 3
+//lRUCache.get(4);    // 返回 4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10000 
+// 0 <= value <= 105 
+// 最多调用 2 * 105 次 get 和 put 
+// 
+// Related Topics 设计 哈希表 链表 双向链表 
+// 👍 2425 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 146
+ */
+public class LruCache {
+    public static void main(String[] args) {
+
+        LruCache lruCache = new LruCache();
+        lruCache.test();
+
+    }
+
+    void test() {
+        LRUCache lruCache = new LRUCache(1);
+        lruCache.put(2, 1);
+        System.out.println(lruCache.get(2));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class LRUCache {
+        private Map<Integer, Integer> keyMap;
+        private Object eldestKey;
+
+        public LRUCache(int capacity) {
+            keyMap = new LinkedHashMap<Integer, Integer>(capacity, .75F, true) {
+                private static final long serialVersionUID = 4267176411845948333L;
+
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                    boolean tooBig = size() > capacity;
+                    if (tooBig) {
+                        eldestKey = eldest.getKey();
+                    }
+                    return tooBig;
+                }
+            };
+        }
+
+        public int get(int key) {
+            Integer integer = keyMap.get(key);
+            if (Objects.isNull(integer)) {
+                return -1;
+            }
+            return integer;
+        }
+
+        public void put(int key, int value) {
+            keyMap.put(key, value);
+        }
+    }
+
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.get(key);
+     * obj.put(key,value);
+     */
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
