1b781058ec6aa76cd3a93e156f6e91ba6fb9b8ba
coy0725 <799057410@qq.com>
2022-12-23 00:35:59 +0800

    235. 二叉搜索树的最近公共祖先 第三遍 我好像更喜欢这个

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -94,6 +94,25 @@ public class LowestCommonAncestorOfABinarySearchTree {
             }
         }
     }
+
+    class Solution3 {
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (p.val==root.val){
+                return p;
+            }
+            if (q.val==root.val){
+                return q;
+            }
+            if (p.val > root.val && q.val > root.val) {
+                return lowestCommonAncestor(root.right,p,q);
+            }else if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left,p,q);
+            }else{
+                return root;
+            }
+        }
+    }
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
