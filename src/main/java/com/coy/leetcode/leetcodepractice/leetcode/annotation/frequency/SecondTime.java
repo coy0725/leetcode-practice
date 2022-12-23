443ad35b4855196d1f496691bfaf15e09d94dd01
coy0725 <799057410@qq.com>
2022-12-23 10:22:24 +0800

    add frequency annotation 1-5

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/annotation/frequency/SecondTime.java
@@ -0,0 +1,9 @@
+package com.coy.leetcode.leetcodepractice.leetcode.annotation.frequency;
+
+/**
+ * 第二次
+ * @author coy
+ * @since 2022/12/23
+ **/
+public @interface SecondTime {
+}
---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/annotation/frequency/ThirdTime.java
@@ -0,0 +1,9 @@
+package com.coy.leetcode.leetcodepractice.leetcode.annotation.frequency;
+
+/**
+ * 第三次
+ * @author coy
+ * @since 2022/12/23
+ **/
+public @interface ThirdTime {
+}
