08a0c557d102a8cc9d4be2023a58159439ebe6d6
coy0725 <799057410@qq.com>
2022-12-24 22:08:20 +0800

    889. 根据前序和后序遍历构造二叉树 找到了 add document

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -76,6 +76,7 @@ public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
      * }
      */
     class Solution {
+        // 存储 postorder 中值到索引的映射
         HashMap<Integer, Integer> valToIndex = new HashMap<>();
 
         public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
@@ -86,7 +87,9 @@ public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
                 postorder, 0, postorder.length - 1);
         }
 
-        private TreeNode build(int[] preorder, int preStart, int preEnd,
+        // 定义：根据 preorder[preStart..preEnd] 和 postorder[postStart..postEnd]
+        // 构建二叉树，并返回根节点。
+        TreeNode build(int[] preorder, int preStart, int preEnd,
             int[] postorder, int postStart, int postEnd) {
             if (preStart > preEnd) {
                 return null;
@@ -94,14 +97,22 @@ public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
             if (preStart == preEnd) {
                 return new TreeNode(preorder[preStart]);
             }
+
+            // root 节点对应的值就是前序遍历数组的第一个元素
             int rootVal = preorder[preStart];
-
-            //怎么确定左子树的大小呢？前序遍历结果的第二个元素作为左子树的根节点的值
-
+            // root.left 的值是前序遍历第二个元素
+            // 通过前序和后序遍历构造二叉树的关键在于通过左子树的根节点
+            // 确定 preorder 和 postorder 中左右子树的元素区间
             int leftRootVal = preorder[preStart + 1];
+            // leftRootVal 在后序遍历数组中的索引
             int index = valToIndex.get(leftRootVal);
+            // 左子树的元素个数
             int leftSize = index - postStart + 1;
+
+            // 先构造出当前根节点
             TreeNode root = new TreeNode(rootVal);
+            // 递归构造左右子树
+            // 根据左子树的根节点索引和元素个数推导左右子树的索引边界
             root.left = build(preorder, preStart + 1, preStart + leftSize,
                 postorder, postStart, index);
             root.right = build(preorder, preStart + leftSize + 1, preEnd,
@@ -110,6 +121,7 @@ public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
             return root;
         }
     }
+
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
