873b06da3d9633a3f3020acd11f05ceeef6975a0
coy0725 <799057410@qq.com>
2022-12-25 09:08:39 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II 执行用时：3 ms, 在所有 Java 提交中击败了1.12%的用户 这么差劲

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/CongShangDaoXiaDaYinErChaShuIiLcof.java
@@ -0,0 +1,102 @@
+/**
+* 从上到下打印二叉树 II
+* 
+*/
+//从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。 
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+//
+// 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-tra
+//versal/ 
+// Related Topics 树 广度优先搜索 二叉树 
+// 👍 256 👎 0
+	
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+import com.coy.leetcode.leetcodepractice.leetcode.annotation.difficulty.Easy;
+import com.coy.leetcode.leetcodepractice.leetcode.annotation.topic.Tree;
+
+/**
+* 剑指 Offer 32 - II
+*/
+@Easy
+@Tree
+public class CongShangDaoXiaDaYinErChaShuIiLcof {
+    public static void main(String[] args) {
+        Solution solution = new CongShangDaoXiaDaYinErChaShuIiLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> rlt = new ArrayList<>();
+        if (root == null) {
+            return rlt;
+        }
+        //使用两个队列来存储当前层和下一层要遍历的元素
+        Queue<TreeNode> currentLayer = new LinkedList<>();
+        Queue<TreeNode> nextLayer = new LinkedList<>();
+        currentLayer.offer(root);
+        rlt.add(currentLayer.stream().map(item->item.val).collect(Collectors.toList()));
+        while (!currentLayer.isEmpty() && currentLayer.peek() != null) {
+            TreeNode node = currentLayer.poll();
+            if (node.left != null) {
+                nextLayer.offer(node.left);
+            }
+            if (node.right != null) {
+                nextLayer.offer(node.right);
+            }
+            if (currentLayer.isEmpty()&&!nextLayer.isEmpty()) {
+                rlt.add(nextLayer.stream().map(item->item.val).collect(Collectors.toList()));
+                Queue<TreeNode> tmp = currentLayer;
+                currentLayer = nextLayer;
+                nextLayer = tmp;
+            }
+        }
+        return rlt;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
