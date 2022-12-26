ab877350a223f9df36b15d1ec0750713e25f9fbb
coy0725 <799057410@qq.com>
2022-12-26 18:01:30 +0800

    35. 搜索插入位置 ac 1

---
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/SearchInsertPosition.java
@@ -0,0 +1,79 @@
+/**
+ * 搜索插入位置
+ */
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// nums 为 无重复元素 的 升序 排列数组 
+// -104 <= target <= 104 
+// 
+// Related Topics 数组 二分查找 
+// 👍 1827 👎 0
+
+package com.coy.leetcode.leetcodepractice.leetcode.editor.cn;
+
+import com.coy.leetcode.leetcodepractice.leetcode.annotation.difficulty.Easy;
+import com.coy.leetcode.leetcodepractice.leetcode.annotation.topic.BinarySearch;
+
+/**
+ * 35
+ */
+@Easy
+@BinarySearch
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Solution solution = new SearchInsertPosition().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
