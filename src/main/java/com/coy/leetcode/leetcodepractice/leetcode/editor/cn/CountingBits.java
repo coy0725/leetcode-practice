6b495a70c9911fba53344390c3724d45ca6ab7b4
coy0725 <799057410@qq.com>
2022-12-30 09:17:53 +0800

    338. 比特位计数 第二遍

--- src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/CountingBits.java
+++ src/main/java/com/coy/leetcode/leetcodepractice/leetcode/editor/cn/CountingBits.java
@@ -92,8 +92,14 @@ public class CountingBits {
     }
 
     class Solution2 {
+        //对于所有的数字，只有奇数和偶数两种：
+        //
+        //奇数：二进制表示中，奇数一定比前面那个偶数多一个 1，因为多的就是最低位的 1。
+        //偶数：二进制表示中，偶数中 1 的个数一定和除以 2 之后的那个数一样多。因为最低位是 0，
+        //除以 2 就是右移一位，也就是把那个 0 抹掉而已，所以 1 的个数是不变的。
+
         //状态转移方程：if i是奇数 rlt[i] = rlt[i-1]+1
-        //             if i是偶数 rlt[i] = i/2
+        //             if i是偶数 rlt[i] = rlt[i/2]
         public int[] countBits(int n) {
             int[] rlt = new int[n + 1];
             rlt[0] = 0;
@@ -106,7 +112,7 @@ public class CountingBits {
 
                 //如果是偶数
                 if (i % 2 == 0) {
-                    rlt[i] = i / 2;
+                    rlt[i] = rlt[i/2];
                 }
             }
 
